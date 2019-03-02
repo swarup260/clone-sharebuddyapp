1ec947e8bb66206cd38fb120008a8c58030c9033
swarup <swarup260@gmail.com>
2019-03-02 22:50:56 +0530

    user setting option field added

--- lib/tabs/setting.dart
+++ lib/tabs/setting.dart
@@ -1,15 +1,241 @@
 import 'package:flutter/material.dart';
 
 class SettingTab extends StatelessWidget {
+  List<Widget> _list = [
+    SettingField(label_name: 'Edit Account'),
+    ReceiveNotification(),
+    SettingField(label_name: 'Login'),
+    SettingField(label_name: 'Send Feedback'),
+    SettingField(label_name: 'Rate Us')
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Setting'),
-      ),
-      body: Center(
-        child: Text('Welcome to Setting'),
-      ),
+      body: NestedSetting(list: _list),
     );
   }
 }
+
+class Setting extends StatelessWidget {
+  const Setting({
+    Key key,
+    @required List<String> list,
+  })  : _list = list,
+        super(key: key);
+
+  final List<String> _list;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          pinned: true,
+          snap: false,
+          floating: true,
+          expandedHeight: 300.0,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Align(
+              child: Text(
+                'Anonymous',
+                style: TextStyle(letterSpacing: 1),
+              ),
+              alignment: Alignment.bottomLeft,
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.person),
+              tooltip: 'User',
+              onPressed: () {},
+            )
+          ],
+        ),
+        SliverFixedExtentList(
+          itemExtent: 50.0,
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return Padding(
+              child: Container(
+                child: Text(
+                  _list[index],
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              padding: EdgeInsets.all(10),
+            );
+          }, childCount: 5),
+        ),
+      ],
+    );
+  }
+}
+
+class NestedSetting extends StatelessWidget {
+  final List<Widget> _list;
+  NestedSetting({
+    Key key,
+    @required List<Widget> list,
+  })  : _list = list,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 250,
+              floating: false,
+              pinned: true,
+              snap: false,
+              flexibleSpace: FlexibleSpaceBar(
+                titlePadding: EdgeInsets.all(10),
+                title: Text('Anonymous'),
+              ),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.person),
+                  tooltip: 'User',
+                  onPressed: () {},
+                )
+              ],
+            )
+          ];
+        },
+        body: ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: ListView.builder(
+            itemCount: _list.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _list[index];
+            },
+          ),
+        ));
+  }
+}
+
+class ReceiveNotification extends StatefulWidget {
+  final Widget child;
+
+  ReceiveNotification({Key key, this.child}) : super(key: key);
+
+  _ReceiveNotificationState createState() => _ReceiveNotificationState();
+}
+
+class _ReceiveNotificationState extends State<ReceiveNotification> {
+  @override
+  bool enabled = false;
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Flexible(
+          child: Padding(
+            child: Text(
+              'Receive Notification',
+              style: TextStyle(fontSize: 18),
+            ),
+            padding: EdgeInsets.all(10),
+          ),
+        ),
+        Flexible(
+          child: Switch(
+            value: enabled,
+            onChanged: (bool enabled) {
+              setState(() {
+                enabled = true;
+              });
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class SettingField extends StatelessWidget {
+  final String label_name;
+  SettingField({Key key, @required String label_name})
+      : label_name = label_name,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Function onTap2 = () {
+      switch (label_name) {
+        case 'Send Feedback':
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Text('Send FeedBack'),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Icon(Icons.send),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                );
+              });
+          break;
+        case 'Rate Us':
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Icon(Icons.star),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Icon(Icons.check_circle),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                );
+              });
+          break;
+        default:
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text('tap working'),
+          ));
+      }
+    };
+    return GestureDetector(
+      child: Padding(
+        child: Container(
+          child: Text(
+            label_name,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        padding: EdgeInsets.all(10),
+      ),
+      onTap: onTap2,
+    );
+  }
+}
+
+// Row(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: <Widget>[
+//           Container(
+//             child: Text(
+//               'Receive Notification',
+//               style: TextStyle(fontSize: 20),
+//             ),
+//             height: 30,
+//           ),
+//           Switch(
+//             value: enabled,
+//             onChanged: (bool enabled) {
+//               setState(() {
+//                 enabled = true;
+//               });
+//             },
+//           )
+//         ],
+//       )
