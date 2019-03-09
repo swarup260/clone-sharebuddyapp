1ac8fe8786a5575d26491664f45ba4db11863500
Rahul Shukla <16shuklarahul@gmail.com>
2019-03-09 20:19:58 +0530

    Map issue solved & list page code structure changes

--- lib/tabs/list.dart
+++ lib/tabs/list.dart
@@ -12,98 +12,8 @@ class ListTab extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Container(
-                      height: 180.0,
-                      width: double.infinity,
-                      color: Colors.yellow,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SizedBox(height: 15.0),
-                        Padding(
-                          padding: EdgeInsets.only(left: 15.0, right: 15.0),
-                          child: Material(
-                            elevation: 5.0,
-                            borderRadius: BorderRadius.circular(20.0),
-                            child: TextFormField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    prefixIcon: Icon(Icons.my_location,
-                                        color: Colors.black, size: 30.0),
-                                    contentPadding:
-                                        EdgeInsets.only(left: 15.0, top: 15.0),
-                                    hintText: 'Source',
-                                    hintStyle: TextStyle(
-                                        color: Colors.grey,
-                                        fontFamily: 'Quicksand'))),
-                          ),
-                        ),
-                        SizedBox(height: 5.0),
-                        Padding(
-                          padding: EdgeInsets.only(left: 15.0, right: 15.0),
-                          child: Material(
-                            elevation: 5.0,
-                            borderRadius: BorderRadius.circular(20.0),
-                            child: TextFormField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    prefixIcon: Icon(Icons.place,
-                                        color: Colors.black, size: 30.0),
-                                    contentPadding:
-                                        EdgeInsets.only(left: 15.0, top: 15.0),
-                                    hintText: 'Destination',
-                                    hintStyle: TextStyle(
-                                        color: Colors.grey,
-                                        fontFamily: 'Quicksand'))),
-                          ),
-                        ),
-                        SizedBox(height: 10.0),
-                        Padding(
-                          padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                          child: Center(
-                            child: RaisedButton(
-                                textColor: Colors.white,
-                                color: Colors.black,
-                                child: Text("Search"),
-                                onPressed: () {},
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30.0))),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                itemCard(
-                    'RAWALPADA',
-                    'BORIVAI RAILWAY STATION',
-                    '(RAWALPADA 297 BUS STOP)',
-                    '10',
-                    '(4KM)',
-                    'assets/images/auto.png'),
-                itemCard('MINDSPACE MALAD', 'MADAL RAILWAY STATION',
-                    '(INTERFACE 16)', '12', '(5KM)', 'assets/images/taxi.png'),
-                itemCard(
-                    'BORIVAI RAILWAY STATION',
-                    'RAWALPADA',
-                    '(BORIVALI 297 BUS STOP)',
-                    '15',
-                    '(4KM)',
-                    'assets/images/auto.png'),
-                itemCard(
-                    'RAWALPADA',
-                    'BORIVAI RAILWAY STATION',
-                    '(RAWALPADA 297 BUS STOP)',
-                    '10',
-                    '(4KM)',
-                    'assets/images/auto.png'),
-              ],
-            ),
+            SearchPanel(),
+            SearchResultPanel(),
           ],
         ),
       ),
@@ -111,9 +21,107 @@ class ListTab extends StatelessWidget {
   }
 }
 
-Widget itemCard(from, to, landmark, price, kilometer, type) {
+/* Search Section */
+class SearchPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 180.0,
+          width: double.infinity,
+          color: Colors.yellow,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 15.0),
+            Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0),
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(20.0),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.my_location,
+                            color: Colors.black, size: 30.0),
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
+                        hintText: 'Source',
+                        hintStyle: TextStyle(
+                            color: Colors.grey, fontFamily: 'Quicksand'))),
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0),
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(20.0),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon:
+                            Icon(Icons.place, color: Colors.black, size: 30.0),
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
+                        hintText: 'Destination',
+                        hintStyle: TextStyle(
+                            color: Colors.grey, fontFamily: 'Quicksand'))),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Padding(
+              padding: EdgeInsets.only(left: 0.0, right: 0.0),
+              child: Center(
+                child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.black,
+                    child: Text("Search"),
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0))),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+/* Result Section */
+class SearchResultPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ResultCard(
+            'RAWALPADA',
+            'BORIVAI RAILWAY STATION',
+            '(RAWALPADA 297 BUS STOP)',
+            '10',
+            '(4KM)',
+            'assets/images/auto.png'),
+        ResultCard('MINDSPACE MALAD', 'MADAL RAILWAY STATION', '(INTERFACE 16)',
+            '12', '(5KM)', 'assets/images/taxi.png'),
+        ResultCard('BORIVAI RAILWAY STATION', 'RAWALPADA',
+            '(BORIVALI 297 BUS STOP)', '15', '(4KM)', 'assets/images/auto.png'),
+        ResultCard(
+            'RAWALPADA',
+            'BORIVAI RAILWAY STATION',
+            '(RAWALPADA 297 BUS STOP)',
+            '10',
+            '(4KM)',
+            'assets/images/auto.png'),
+      ],
+    );
+  }
+}
+
+/* Search Result Card */
+Widget ResultCard(from, to, landmark, price, kilometer, type) {
   return Padding(
-    padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0),
+    padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0),
     child: Card(
         child: Container(
       height: 120.0,
