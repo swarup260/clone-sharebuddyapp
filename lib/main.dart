c15f937e6f7f5f3a0f7a75a68bf8c9c42ccd135e
swarup <swarup260@gmail.com>
2019-03-24 23:02:33 +0530

    routes object remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:share_buddy/tabs/screens/edit_account_screen.dart';
-import 'package:share_buddy/tabs/screens/login_scrren.dart';
-import 'package:share_buddy/tabs/screens/register_screen.dart';
 import 'home.dart';
 
 void main() => runApp(MyApp());
@@ -20,12 +17,7 @@ class MyApp extends StatelessWidget {
         indicatorColor: Colors.black,
         fontFamily: 'Rubik',
       ),
-      home: Home(),
-      routes: {
-        '/Login': (_) => LoginScreen(),
-        '/Register': (_) => RegisterScreen(),
-        '/Edit Account': (_) => EditAccountScreen()
-      },
+      home: Home()
     );
   }
 }
