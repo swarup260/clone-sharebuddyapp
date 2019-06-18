d142c03f2b2a0bea7fb728672be5b4a4ead55378
swarup <swarup260@gmail.com>
2019-06-18 22:16:22 +0530

    condition added to show walkthrough screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:admob_flutter/admob_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'api/apiEndpoint.dart';
+import './tabs/screens/walk_through.dart';
+import 'api/networkManager.dart';
 import 'home.dart';
 
 void main() {
@@ -23,7 +26,23 @@ class MyApp extends StatelessWidget {
         indicatorColor: Colors.black,
         fontFamily: 'Rubik',
       ),
-      home: Home(),
+      // home: Home(),
+      home: FutureBuilder(
+        future: isShowWalkThrough(),
+        initialData: false,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data) {
+            return Home();
+          } else {
+            return Walkthrough();
+          }
+        },
+      ),
     );
+  }
+
+  Future<bool> isShowWalkThrough() async {
+    final SharedPreferences prefs = await getPrefs();
+    return prefs.getBool("walkThroughView") ?? false;
   }
 }
